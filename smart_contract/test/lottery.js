6888e438ea26f4ba5d187e3b8edb4539017dae21
Jaewoneeee <93761302+Jaewoneeee@users.noreply.github.com>
2022-08-04 22:04:11 +0900

    lec 2day

---
+++ smart_contract/test/lottery.js
@@ -0,0 +1,11 @@
+const Lottery = artifacts.require("Lottery");
+
+contract('Lottery', function( [deployer, user1, user2] ){
+    beforeEach(async () => {
+        console.log('before each')
+    })
+
+    it('Basic test', async () => {
+        console.log('basic test')
+    })
+})
